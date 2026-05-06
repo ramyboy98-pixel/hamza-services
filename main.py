748729e4eaee4b2367009097d9a505d4277d1b63
ramyboy98-pixel <ramyboy98@gmail.com>
2026-05-06 23:53:38 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,48 @@
-print("HAMZA SERVICES")
+import tkinter as tk
+
+root = tk.Tk()
+
+root.title("HAMZA SERVICES")
+root.geometry("1280x720")
+root.configure(bg="#2b2b2b")
+
+title = tk.Label(
+    root,
+    text="HAMZA SERVICES",
+    font=("Arial", 36, "bold"),
+    fg="white",
+    bg="#2b2b2b"
+)
+title.pack(pady=40)
+
+frame = tk.Frame(root, bg="#2b2b2b")
+frame.pack(expand=True)
+
+buttons = [
+    "وثائق",
+    "خدمات إلكترونية",
+    "أرشيف",
+    "إعدادات"
+]
+
+for text in buttons:
+    card = tk.Frame(
+        frame,
+        bg="#3a3a3a",
+        width=220,
+        height=220
+    )
+    card.pack(side="left", padx=25)
+
+    card.pack_propagate(False)
+
+    label = tk.Label(
+        card,
+        text=text,
+        font=("Arial", 22, "bold"),
+        fg="white",
+        bg="#3a3a3a"
+    )
+    label.pack(expand=True)
+
+root.mainloop()
